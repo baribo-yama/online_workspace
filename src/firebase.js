e14619aed13ee5488cf737abfb07f72e14f35d95
baribo-yama <kenta6flat@icloud.com>
2025-09-10 20:03:54 +0900

    firebaseのキーをenvに格納

--- src/firebase.js
+++ src/firebase.js
@@ -7,19 +7,19 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDXZxQzJ3ldiySsoGCKiZ_nmFSalr9nxPI",
-  authDomain: "online-workspace-1c2a4.firebaseapp.com",
-  projectId: "online-workspace-1c2a4",
-  storageBucket: "online-workspace-1c2a4.firebasestorage.app",
-  messagingSenderId: "245907207992",
-  appId: "1:245907207992:web:d973906089f0d03380b3cd",
-  measurementId: "G-X1YW2WDF5L"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-console.log("hello firebase")
+
 
 // Firestoreを使えるようにexport
 export const db = getFirestore(app);
