dc81f4e986020bfd32f414c43f5c2f13acae369f
baribo-yama <kenta6flat@icloud.com>
2025-09-10 19:36:48 +0900

    cssをホーム画面に適用。ポモドーロタイマーをコンポーネントにまとめた

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './globals.css'
-import StudyRoomApp from './StudyRoomApp.jsx'
 // firestoreのテスト用でApp.jsxを呼び出し
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <StudyRoomApp /> */}
     <App />
   </StrictMode>,
 )
