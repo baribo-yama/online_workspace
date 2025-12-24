7cfbccfb43b750a4b354e8d60a096d811b634bcc
tnom0524 <tnom0524@gmail.com>
2025-12-24 14:53:12 +0900

    done send notify to one ch

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
-const {onRequest} = require("firebase-functions/v2/https");
-const {createLivekitToken} = require("./livekit/token");
+const { onRequest } = require("firebase-functions/v2/https");
+const { createLivekitToken } = require("./livekit/token");
 
 // Firebase Admin SDK初期化
 admin.initializeApp();
@@ -14,11 +14,7 @@ exports.sendSlackNotification = onRequest(
     region: "asia-northeast1",
     timeoutSeconds: 10,
     memory: "256MiB",
-    cors: [
-      "https://online-workspace-1c2a4.web.app",
-      "https://online-workspace-1c2a4.firebaseapp.com",
-      /^http:\/\/localhost:\d+$/,
-    ],
+    cors: true,
   },
   notifyModule.handler,
 );
